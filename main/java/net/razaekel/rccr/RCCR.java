e74cfa9c92fac8e018e4391508d7ad7b824319cf
Razaekel <ubersentinel@gmail.com>
2014-04-13 09:49:51 -0400

    First commit, initial mod descriptor files

---
+++ main/java/net/razaekel/rccr/RCCR.java
@@ -0,0 +1,24 @@
+package net.razaekel.rccr;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = RCCR.MODID, version = RCCR.VERSION)
+public class RCCR
+{
+    public static final String MODID = "rccr";
+    public static final String VERSION = "a1";
+    
+    @Instance(value = "rccr")
+    public static RCCR instance;
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
