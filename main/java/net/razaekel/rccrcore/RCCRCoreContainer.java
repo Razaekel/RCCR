14f6a8e6645924e96e11e672a4321403eee20562
Razaekel <ubersentinel@gmail.com>
2014-04-14 00:37:36 -0400

    expanded core container

--- main/java/net/razaekel/rccrcore/RCCRCoreContainer.java
+++ main/java/net/razaekel/rccrcore/RCCRCoreContainer.java
@@ -2,14 +2,19 @@ package net.razaekel.rccrcore;
 
 import java.util.Arrays;
 
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.DummyModContainer;
 import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.ModMetadata;
-import cpw.mods.fml.common.eventhandler.EventBus;
-
+import cpw.mods.fml.common.event.FMLConstructionEvent;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class RCCRCoreContainer extends DummyModContainer
 {
@@ -32,9 +37,30 @@ public class RCCRCoreContainer extends DummyModContainer
 		meta.logoFile = "";
 	}
 	
+	@Override
 	public boolean registerBus(EventBus bus, LoadController controller)
 	{
 		bus.register(this);
 		return true;
+	}
+
+	@Subscribe
+	public void modConstruction(FMLConstructionEvent evt){
+
+	}
+
+	@Subscribe
+	public void init(FMLInitializationEvent evt) {
+
+	}
+	
+	@Subscribe
+	public void preInit(FMLPreInitializationEvent evt) {
+
+	}
+
+	@Subscribe
+	public void postInit(FMLPostInitializationEvent evt) {
+
 	}
 }
