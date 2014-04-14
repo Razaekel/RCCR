f77a6cc561fa92e6194ab66cfb65e2cac7f09d90
Razaekel <ubersentinel@gmail.com>
2014-04-14 01:22:52 -0400

    for some reason, the @MOD line makes minecraft unable to find the mojang logo file, the glyph_sizes file, and the glyph font file

--- main/java/mod/razaekel/rccr/RCCR.java
+++ main/java/mod/razaekel/rccr/RCCR.java
@@ -1,12 +1,10 @@
 package mod.razaekel.rccr;
 
-import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = RCCR.MODID, version = RCCR.VERSION)
 public class RCCR
 {
     public static final String MODID = "rccr";
@@ -18,7 +16,5 @@ public class RCCR
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
 }
