a896f1ab22656bd2096f0e6c6d0cfdba7da1be55
Razaekel <ubersentinel@gmail.com>
2014-04-14 00:37:48 -0400

    added loading plugin

---
+++ mod/razaekel/rccrcore/RCCRLoadingPlugin.java
@@ -0,0 +1,37 @@
+package mod.razaekel.rccrcore;
+
+import java.util.Map;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+public class RCCRLoadingPlugin implements IFMLLoadingPlugin {
+
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[]{RCCRClassTransformer.class.getName()};
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return RCCRCoreContainer.class.getName();
+	}
+
+	@Override
+	public String getSetupClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getAccessTransformerClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
