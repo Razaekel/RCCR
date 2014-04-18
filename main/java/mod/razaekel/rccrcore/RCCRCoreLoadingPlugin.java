b5c87a596a3aa66b65cb829a7fbbe1a61414350d
Razaekel <ubersentinel@gmail.com>
2014-04-18 14:36:47 -0400

    renamed

--- main/java/mod/razaekel/rccrcore/RCCRCoreLoadingPlugin.java
+++ main/java/mod/razaekel/rccrcore/RCCRCoreLoadingPlugin.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
-public class RCCRLoadingPlugin implements IFMLLoadingPlugin
+public class RCCRCoreLoadingPlugin implements IFMLLoadingPlugin
 {
 	@Override
 	public String[] getASMTransformerClass()
