a7d6a105d7c636deb8d5d112d72a148684d1bd2d
Razaekel <ubersentinel@gmail.com>
2014-04-18 12:56:08 -0400

    Revert "added debug messages"
    This reverts commit fb69613a554024e60e3636ef35e8fad7f4a1957b.

--- main/java/mod/razaekel/rccrcore/RzClassTransformerClass.java
+++ main/java/mod/razaekel/rccrcore/RzClassTransformerClass.java
@@ -2,8 +2,6 @@ package mod.razaekel.rccrcore;
 
 import java.util.ArrayList;
 
-import mod.razaekel.rccr.RCCR;
-
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
 
@@ -27,17 +25,12 @@ public abstract class RzClassTransformerClass extends RzClassTransformer
 		boolean sigs[] = new boolean[registeredMethods.size()];
 		
 		for (MethodNode m : classNode.methods)
-		{			
+		{
 			for (int methodIndex = 0; methodIndex < registeredMethods.size(); methodIndex++)
 			{
 				String[] methodInfo = registeredMethods.get(methodIndex);
-				
-				if (RCCR.RCCR_DEBUG){System.out.println("Inspecting " + m.name + "!! Looking for desc: " + m.desc +" using " + methodInfo[1] +"!");}
-				
 				String srgName = getSrgName(className, m);
 				String srgSignature = getSrgDescriptor(m.desc);
-				
-				if (RCCR.RCCR_DEBUG){System.out.println("Getting " + srgName + " for " + className + " and " + srgSignature + " for " + m.desc +"!");}
 				
 				if (srgName.equals(methodInfo[0]) && srgSignature.equals(methodInfo[1]))
 				{
@@ -56,7 +49,7 @@ public abstract class RzClassTransformerClass extends RzClassTransformer
 			if (!sigs[methodIndex])
 			{
 				String[] methodInfo = registeredMethods.get(methodIndex);
-				RCCRCoreContainer.logger.error("Could not transform expected method in class \"" + className + "\" (Obf: " + obf + "): " + methodInfo[0] + " - " + methodInfo[1] + " - " + methodInfo[2]);
+				 RCCRCoreContainer.logger.error("Could not transform expected method in class \"" + className + "\" (Obf: " + obf + "): " + methodInfo[0] + " - " + methodInfo[1] + " - " + methodInfo[2]);
 			}
 			else
 			{
