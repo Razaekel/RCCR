931554c93fa198e5d01aa314ac03c67f2a7784e0
Razaekel <ubersentinel@gmail.com>
2014-04-20 16:45:58 -0400

    added SIPUSH128

--- main/java/mod/razaekel/rccrcore/lib/InsnTransformers.java
+++ main/java/mod/razaekel/rccrcore/lib/InsnTransformers.java
@@ -110,4 +110,49 @@ public class InsnTransformers
 		
 		return true;
 	}
+	
+	public static boolean replaceSIPUSH128(MethodNode methodNode, int targetIndex)
+	{
+		AbstractInsnNode currentNode = null;
+		AbstractInsnNode targetNode = null;
+
+		@SuppressWarnings("unchecked")
+		Iterator<AbstractInsnNode> iter = methodNode.instructions.iterator();
+
+		int index = -1;
+		int node_index = -1;
+
+		while (iter.hasNext())
+		{
+			index++;
+			currentNode = iter.next();
+
+			if (currentNode.getOpcode() == SIPUSH && ((IntInsnNode) currentNode).operand == 128)
+			{
+				targetNode = currentNode;
+				node_index = index;
+			}
+		}
+
+		if(RCCRCore.DEBUG){System.out.println("********* node_index should be " + targetIndex + " -> " + node_index);}
+
+		if(RCCRCore.DEBUG)
+		{
+			if (targetNode == null || node_index == -1)
+			{
+				System.out.println("Did not find all necessary target nodes! ABANDON CLASS!");
+				return false;
+			}
+		}
+
+		AbstractInsnNode remNode1 = methodNode.instructions.get(node_index);
+
+		InsnList toInject = new InsnList();
+		toInject.add(new MethodInsnNode(INVOKESTATIC, "mod/razaekel/rccr/RCCR", "getNoSkyWorldHeight", Type.getMethodDescriptor(Type.INT_TYPE)));
+		methodNode.instructions.insert(targetNode, toInject);
+
+		methodNode.instructions.remove(remNode1);
+		
+		return true;
+	}
 }
