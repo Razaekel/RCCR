23c3fa0eb9c8f0f42135c6a3db81eff2d112e86a
Razaekel <ubersentinel@gmail.com>
2014-04-20 16:47:44 -0400

    added World Provider transformer to adjust for forge's modifications

---
+++ main/java/mod/razaekel/rccrcore/transformers/WorldProviderTransformer.java
@@ -0,0 +1,57 @@
+package mod.razaekel.rccrcore.transformers;
+
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.MethodNode;
+
+import mod.razaekel.rccrcore.RCCRCore;
+import mod.razaekel.rccrcore.RzClassTransformerClass;
+import mod.razaekel.rccrcore.lib.InsnTransformers;
+
+public class WorldProviderTransformer extends RzClassTransformerClass
+{
+	public WorldProviderTransformer()
+	{
+		registerExpectedMethod("getHeight", "getHeight", getMethodDescriptor(Type.INT_TYPE));
+		registerExpectedMethod("getActualHeight", "getActualHeight", getMethodDescriptor(Type.INT_TYPE));
+	}
+	
+	@Override
+	public boolean transformMethod(String className, String methodID, MethodNode methodNode, boolean obf)
+	{		
+		if(RCCRCore.DEBUG_SPAM){System.out.println("WORLD PROVIDER TRANSFORMER LOADED! BEGINNING TRANSFORM!");}
+		
+		if(methodID.equals("getHeight"))
+		{
+			if(RCCRCore.DEBUG){System.out.println("********* Inside target method " + methodID + "!");}
+			
+			if(!InsnTransformers.replaceSIPUSH256(methodNode, 21))
+			{
+				return false;
+			}
+	
+			if(RCCRCore.DEBUG){System.out.println("Patching Complete!");}
+			
+			return true;
+		}
+		else if(methodID.equals("getActualHeight"))
+		{
+			if(RCCRCore.DEBUG){System.out.println("********* Inside target method " + methodID + "!");}
+			
+			if(!InsnTransformers.replaceSIPUSH128(methodNode, 21))
+			{
+				return false;
+			}
+			
+			if(!InsnTransformers.replaceSIPUSH256(methodNode, 21))
+			{
+				return false;
+			}
+	
+			if(RCCRCore.DEBUG){System.out.println("Patching Complete!");}
+			
+			return true;
+		}
+		
+		return false;
+	}
+}
