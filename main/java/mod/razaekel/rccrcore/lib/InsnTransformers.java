b807204e2f848ccd6774c6043a8eb92b58eefa1e
Razaekel <ubersentinel@gmail.com>
2014-04-20 21:40:54 -0400

    cleaned up

--- main/java/mod/razaekel/rccrcore/lib/InsnTransformers.java
+++ main/java/mod/razaekel/rccrcore/lib/InsnTransformers.java
@@ -1,21 +1,15 @@
 package mod.razaekel.rccrcore.lib;
 
-import static org.objectweb.asm.Opcodes.ICONST_1;
-import static org.objectweb.asm.Opcodes.INVOKESTATIC;
-import static org.objectweb.asm.Opcodes.ISUB;
-import static org.objectweb.asm.Opcodes.SIPUSH;
+import static org.objectweb.asm.Opcodes.*;
 
 import java.util.Iterator;
 
 import mod.razaekel.rccrcore.RCCRCore;
 
 import org.objectweb.asm.Type;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.IntInsnNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.*;
+
+import com.sun.xml.internal.ws.org.objectweb.asm.Opcodes;
 
 public class InsnTransformers
 {
@@ -149,6 +143,52 @@ public class InsnTransformers
 
 		InsnList toInject = new InsnList();
 		toInject.add(new MethodInsnNode(INVOKESTATIC, "mod/razaekel/rccr/RCCR", "getNoSkyWorldHeight", Type.getMethodDescriptor(Type.INT_TYPE)));
+		methodNode.instructions.insert(targetNode, toInject);
+
+		methodNode.instructions.remove(remNode1);
+		
+		return true;
+	}
+	
+	public static boolean replaceWithWorldHeightInCubes(MethodNode methodNode, int targetIndex)
+	{
+		AbstractInsnNode currentNode = null;
+		AbstractInsnNode targetNode = null;
+
+		@SuppressWarnings("unchecked")
+		Iterator<AbstractInsnNode> iter = methodNode.instructions.iterator();
+
+		int index = -1;
+		int node_index = -1;
+
+		while (iter.hasNext())
+		{
+			index++;
+			currentNode = iter.next();
+
+			if (currentNode.getOpcode() == BIPUSH && ((IntInsnNode) currentNode).operand == 16)
+			{
+				targetNode = currentNode;
+				node_index = index;
+				break;
+			}
+		}
+
+		if(RCCRCore.DEBUG){System.out.println("********* node_index should be " + targetIndex + " -> " + node_index);}
+
+		if(RCCRCore.DEBUG)
+		{
+			if (targetNode == null || node_index == -1)
+			{
+				System.out.println("Did not find all necessary target nodes! ABANDON CLASS!");
+				return false;
+			}
+		}
+
+		AbstractInsnNode remNode1 = methodNode.instructions.get(node_index);
+
+		InsnList toInject = new InsnList();
+		toInject.add(new VarInsnNode(Opcodes.ILOAD, 1));
 		methodNode.instructions.insert(targetNode, toInject);
 
 		methodNode.instructions.remove(remNode1);
